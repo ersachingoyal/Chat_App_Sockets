dc1a72bca6588d07844b7a5ac5e602bd45909f25
Sachin goyal <sachin@thegeeksquad.tech>
2023-07-02 12:46:48 +0530

    Added basic chat app

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ const io = socketio(server);
 io.on('connection', (socket) => { //io.on to is use to listen to emitted events, which is here connection
     console.log('a user connected');
 
-    socket.on('from_client', () => {
-        console.log('event coming from client');
-    })
+    // socket.on('from_client', () => {
+    //     console.log('event coming from client');
+    // })
     
-    setInterval(() => {
-        socket.emit('from_server'); //this will emit events from this side which we can listen on the other side, here the server is sending some event after every 2 seconds
-    }, 2000);
+    // setInterval(() => {
+    //     socket.emit('from_server'); //this will emit events from this side which we can listen on the other side, here the server is sending some event after every 2 seconds
+    // }, 2000);
+
+    socket.on('msg_send', (data) => {
+        io.emit('msg_rcvd', data);  //after recieving the msg, the server will emit this event to all the socket connections 
+    })
 })
 
 app.use('/', express.static(__dirname + '/public')); //using this we can server our static files using express , like html css and other
--- public/script.js
+++ public/script.js
@@ -1,12 +1,30 @@
 var socket = io(); //need to create the socket object
 
+// let btn = document.getElementById('btn');
+// btn.onclick = function exec(){
+//     socket.emit('from_client'); //emiting event from the client
+// }
+
+// socket.on('from_server', () => {  //socket.on is used to listen to the emitted events
+//     const div = document.createElement('div');
+//     div.innerText = 'New Event from Server';
+//     document.body.appendChild(div);
+// })
+
+//the above code was for testing basic setup
+
 let btn = document.getElementById('btn');
+let inputMsg = document.getElementById('newmsg');
+let msgList = document.getElementById('msglist');
+
 btn.onclick = function exec(){
-    socket.emit('from_client'); //emiting event from the client
+    socket.emit('msg_send', {   //we can also send our msg with the emitted event
+        msg: inputMsg.value
+    })
 }
 
-socket.on('from_server', () => {  //socket.on is used to listen to the emitted events
-    const div = document.createElement('div');
-    div.innerText = 'New Event from Server';
-    document.body.appendChild(div);
+socket.on('msg_rcvd', (data) => {
+    let limsg = document.createElement('li');
+    limsg.innerText = data.msg;
+    msgList.appendChild(limsg);
 })
